0863c310fd1d2f9348a6bdfb679cbc60f3e90971
asgk912 <asgk912@gmail.com>
2020-01-09 17:33:04 -0800

    made connection server and db

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,11 +1,12 @@
 var Parse = {
 
-  server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
+  // server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
+  server: 'http://localhost:3000',
 
   create: function(message, successCB, errorCB = null) {
-
+    // post messages
     $.ajax({
-      url: Parse.server,
+      url: Parse.server + '/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -14,11 +15,12 @@ var Parse = {
         console.error('chatterbox: Failed to create message', error);
       }
     });
-      },
+  },
 
+  // get all messages
   readAll: function(successCB, errorCB = null) {
     $.ajax({
-      url: Parse.server,
+      url: Parse.server + '/messages',
       type: 'GET',
       data: { order: '-createdAt' },
       contentType: 'application/json',
--- server/schema.sql
+++ server/schema.sql
@@ -1,53 +1,42 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
+
+/* Create other tables and define schemas for them here! */
+-- CREATE TABLE rooms (
+--   id INT(11) AUTO_INCREMENT PRIMARY KEY,
+--   name CHAR(255) NOT NULL UNIQUE
+-- );
+
+CREATE TABLE users (
+  id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(255) NOT NULL UNIQUE
+);
+
+-- CREATE TABLE friends (
+--   user_id INT(11),
+--   friend_id INT(11),
+--   FOREIGN KEY (user_id)
+--     REFERENCES users(id),
+--   FOREIGN KEY (friend_id)
+--     REFERENCES users(id)
+-- );
 
 CREATE TABLE messages (
   /* Describe your table here.*/
   id INT(11) AUTO_INCREMENT PRIMARY KEY,
   text CHAR(140) NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
-);
-
-/* Create other tables and define schemas for them here! */
-CREATE TABLE rooms (
-  id INT(11) AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(255) NOT NULL
-);
-
-CREATE TABLE users (
-  id INT(11) AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(255) NOT NULL,
-  current_room INT(11) NOT NULL,
-  FOREIGN KEY (current_room)
-    REFERENCES rooms(id)
-);
-
-CREATE TABLE friends (
   user_id INT(11),
-  friend_id INT(11),
-  FOREIGN KEY (user_id)
-    REFERENCES users(id),
-  FOREIGN KEY (friend_id)
-    REFERENCES users(id)
+  roomname CHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  -- FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
-
-CREATE TABLE UMRjoin (
-  message_id INT(11),
-  user_id INT(11),
-  room_id INT(11),
-  FOREIGN KEY (message_id)
-    REFERENCES messages(id),
-  FOREIGN KEY (user_id)
-    REFERENCES users(id),
-  FOREIGN KEY (room_id)
-    REFERENCES rooms(id)
-);
-
-
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student -pstudent < server/schema.sql
  *  to create the database and the tables.*/
 
